ab3d0a70c2c47e721fc6faa6d76e9cea159a82a3
remnantcjy <remnantcjy@gmail.com>
2025-10-20 11:42:50 +0900

    feat(level_2): 계산기 Calculator 클래스 기능 추가 - 사칙연산 및 계산 결과를 ArrayList에 더해주는 calculate 메서드 추가

--- src/level_2/Calculator.java
+++ src/level_2/Calculator.java
@@ -5,13 +5,9 @@ import java.util.List;
 
 public class Calculator {
     // 속성 - 변수 private 접근 제어자 설정
-    /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
     private List<Integer> result = new ArrayList<>();
 
-    // 생성자의 특징
-    // 1. 클래스명이 동일하다.
-    // 2. 반환 데이터 타입이 없다.
-    // 3. 여러 개가 존재할 수 있다.
+    // 생성자
 
     // 기능
     // getter
@@ -24,6 +20,26 @@ public class Calculator {
         this.result = result;
     }
 
+    // 사칙연산 수행 및 계산 결과를 컬렉션에 더해주는 메서드
+    public void calculate(int num1, int num2, char operator) {
+
+        switch (operator) {
+            case '+':
+                result.add(num1 + num2);
+                break;
+            case '-':
+                result.add(num1 - num2);
+                break;
+            case '*':
+                result.add(num1 * num2);
+                break;
+            case '/':
+                result.add(num1 / num2);
+                break;
+            default:
+                break;
+        }
+    }
 
 
 }

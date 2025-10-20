3d9bec9ccbbd5f9d41bd1d07aa32d1d3c7c6f6e1
remnantcjy <remnantcjy@gmail.com>
2025-10-20 15:06:23 +0900

    refactor(level_3): 계산기 OperatorType enum 클래스에 맞게 리팩터링 - enum 클래스의 추상 메서드 정의 및 오버라이딩 - 사칙연산자 enum 타입에 맞게 검증 메서드 추가 - 사칙연산 입력 검증에 try - catch문 구현 - switch문에서 enum 타입으로 변환

--- src/level_3/OperatorType.java
+++ src/level_3/OperatorType.java
@@ -1,22 +1,54 @@
 package level_3;
 
 public enum OperatorType {
-    ADD('+'),
-    SUBTRACT('-'),
-    MULTIPLY('*'),
-    DIVIDE('/')
-    ;
+    ADD('+') {
+        @Override
+        public double apply(double num1, double num2) {
+            return num1 + num2;
+        }
+    },
+    SUBTRACT('-') {
+        @Override
+        public double apply(double num1, double num2) {
+            return num1 - num2;
+        }
+    },
+    MULTIPLY('*') {
+        @Override
+        public double apply(double num1, double num2) {
+            return num1 * num2;
+        }
+    },
+    DIVIDE('/') {
+        @Override
+        public double apply(double num1, double num2) {
+            return num1 / num2;
+        }
+    };
 
-    // 속 - private
-    private char operator;
-
+    // 클래스 생성자와 멤버
+    private final char operator;
     // enum - 기본 접근제어자 private (생략 가능)
     OperatorType(char operator) {
         this.operator = operator;
     }
 
-    // 기능
+    // getter
     public char getOperator() {
         return this.operator;
     }
+
+    // 열거 객체의 메소드에 사용될 추상 메서드 정의
+    public abstract double apply(double num1, double num2);
+
+    // 입력된 문자가 유효한 연산자인지 검증하고, 해당 operatorType을 반환
+    public static OperatorType fromChar(char input) {
+        for (OperatorType operatorType : values()) {
+            if (operatorType.getOperator() == input) {
+                return operatorType;
+            }
+        }
+        throw new IllegalArgumentException("잘못된 연산자입니다: " + input);
+    }
 }
+
